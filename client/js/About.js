f940371e082f4a38d7eaed33b2c6ec042dc99aae
Lefonde <alexlef3@gmail.com>
2022-02-19 23:43:23 +0200

    about about

--- client/js/About.js
+++ client/js/About.js
@@ -23,9 +23,43 @@ class About extends React.Component {
                 )
             ),
             React.createElement(
-                "h1",
-                null,
-                "About"
+                "div",
+                { className: "about" },
+                React.createElement(
+                    "h1",
+                    null,
+                    "About"
+                ),
+                React.createElement(
+                    "h2",
+                    null,
+                    "Work of art by:"
+                ),
+                React.createElement(
+                    "h3",
+                    null,
+                    "Alex Lefonde 320774078"
+                ),
+                React.createElement(
+                    "h4",
+                    null,
+                    "alexeyle@mta.ac.il"
+                ),
+                React.createElement(
+                    "h3",
+                    null,
+                    "David Chen 300334034"
+                ),
+                React.createElement(
+                    "h4",
+                    null,
+                    "davidch@mta.ac.il"
+                ),
+                React.createElement(
+                    "p",
+                    null,
+                    "pst... admin - username: admin@gmail.com, password: 12345678"
+                )
             )
         );
     }
--- client/src/About.js
+++ client/src/About.js
@@ -16,7 +16,15 @@ class About extends React.Component{
                     <NavLinks />
                 </nav>
             </header>
-            <h1>About</h1>
+            <div className="about">
+                <h1>About</h1>
+                <h2>Work of art by:</h2>
+                <h3>Alex Lefonde 320774078</h3>
+                <h4>alexeyle@mta.ac.il</h4>
+                <h3>David Chen 300334034</h3>
+                <h4>davidch@mta.ac.il</h4>
+                <p>pst... admin - username: admin@gmail.com, password: 12345678</p>
+            </div>
             </div>
         )
     }
